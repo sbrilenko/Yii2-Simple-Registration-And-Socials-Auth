2fc560c8fa01560f0ee4047388386a43353d8e92
sbrilenko <tip4eg7>
2017-06-14 11:13:23 +0300

    Init Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+Yii 2 Basic Registration and Social Auth Project
+============================
+- by using [https://github.com/amnah/yii2-user](https://github.com/amnah/yii2-user)
+
+# Steps to run
+
+- git clone https://github.com/sbrilenko/Yii2-Simple-Registration-And-Socials-Auth.git
+- cd Yii2-Simple-Registration-And-Socials-Auth
+- composer install
+- edit file config/web.php authClientCollection:
+   - part authClientCollection->clients and add your own socials keys and secrets
---
+++ config/web.php
@@ -0,0 +1,106 @@
+<?php
+
+$params = require(__DIR__ . '/params.php');
+$db = require(__DIR__ . '/db.php');
+
+$config = [
+    'id' => 'basic',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
+    'modules' => [
+        'user' => [
+            'class' => 'dektrium\user\Module',
+            'admins' => ['admin']
+        ],
+    ],
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'ekBdASJDsc4XeWmCWdTmlhs36k_c41uN',
+            'baseUrl' => '',
+        ],
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+//        'user' => [
+//            'identityClass' => 'app\models\User',
+//            'enableAutoLogin' => true,
+//        ],
+        'authClientCollection' => [
+            'class'   => \yii\authclient\Collection::className(),
+            'clients' => [
+                'facebook' => [
+                    'class'        => 'dektrium\user\clients\Facebook',
+                    'clientId'     => '454138578252989',
+                    'clientSecret' => 'dd487bd0fb004aaa7cd5f23c283827e9',
+                ],
+                'twitter' => [
+                    'class' => 'yii\authclient\clients\Twitter',
+                    'consumerKey' => 'gLjgSNgzOaNQtLsDKDs6GrkEs',
+                    'consumerSecret' => '2X2kz2O71HR5ucVyru6lJ2ILHpTjY2FVKQeL7kmeR9fb9vALL7',
+                ],
+                'google' => [
+                    'class' => 'yii\authclient\clients\Google',
+                    'clientId' => '67722942755-ti9d534ratqmbm9u83ldo13v13q4ostf.apps.googleusercontent.com',
+                    'clientSecret' => 'pAbp9n-ORUokLGYcaTZjq19j',
+                ],
+            ],
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@app/mailer',
+            'useFileTransport' => true,
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => 'smtp.gmail.com',
+                'username' => 'brilenkosp',
+                'password' => 'tip4eg007',
+                'port' => '587',
+                'encryption' => 'tls',
+            ],
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'db' => $db,
+
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+                '' => 'site/index',
+                '<action>'=>'site/<action>',
+            ],
+        ],
+
+    ],
+    'params' => $params,
+];
+
+if (YII_ENV_DEV) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+        // uncomment the following to add your IP if you are not connecting from localhost.
+        //'allowedIPs' => ['127.0.0.1', '::1'],
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+        // uncomment the following to add your IP if you are not connecting from localhost.
+        //'allowedIPs' => ['127.0.0.1', '::1'],
+    ];
+}
+
+return $config;
